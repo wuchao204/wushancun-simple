a05856b82c55351713ed2dbb3c83b3105de7252e
wuchao204 <wuchao8204@163.com>
2017-06-07 19:42:42 +0800

    system

---
+++ src/main/java/com/lucky/wushancun/system/controller/SystemController.java
@@ -0,0 +1,10 @@
+package com.lucky.wushancun.system.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by acer on 2017/6/7.
+ */
+@Controller
+public class SystemController {
+}
