72fe2e65be125a13bc74b11db514ad3dc1b81cc0
wuchao204 <wuchao80204@163.com>
2017-06-07 16:51:04 +0800

    spring

--- src/main/java/com/lucky/common/constant/Constant.java
+++ src/main/java/com/lucky/common/constant/Constant.java
@@ -4,5 +4,5 @@ package com.lucky.common.constant;
  * Created by lucky on 2017/6/7.
  */
 public class Constant {
-    public static String USER_SESSION_INFO = "";
+    public static String USER_SESSION_INFO = "USER_SESSION";
 }
