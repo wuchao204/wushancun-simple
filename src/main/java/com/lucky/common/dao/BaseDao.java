14e53bf3216a8e318abd980cc50fb1f3091e728a
wuchao204 <wuchao80204@163.com>
2017-06-07 16:25:23 +0800

    hibernate

---
+++ src/main/java/com/lucky/common/dao/BaseDao.java
@@ -0,0 +1,8 @@
+package com.lucky.common.dao;
+
+/**
+ * Created by lucky on 2017/6/7.
+ */
+public class BaseDao {
+
+}
---
+++ src/main/java/com/lucky/common/util/BaseController.java
@@ -0,0 +1,7 @@
+package com.lucky.common.util;
+
+/**
+ * Created by lucky on 2017/6/7.
+ */
+public class BaseController {
+}
